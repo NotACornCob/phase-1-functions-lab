9fe08f00256cf1d9fa1954ae88695051a63842a8
Alex <alex.kleinman@gmail.com>
2024-05-13 16:29:27 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+
+    let distance;
+
+    if (someValue >= 42){
+        distance = (someValue - 42)
+    }
+    if (someValue < 42){
+        distance = (42 - someValue)
+    }
+    return distance;
+}
+
+function distanceFromHqInFeet(someValue) {
+    let feet = (264 * distanceFromHqInBlocks(someValue));
+    return feet;
+  }
+
+
+function distanceTravelledInFeet(start, destination) {
+    
+    let travelled; 
+    if (start < destination){
+        travelled = (264 * (destination - start)); 
+    }
+    if (start > destination){
+        travelled = (264 * (start - destination)); 
+    }
+    return travelled;
+  }
+
+function calculatesFarePrice(start, destination) {
+    let farePrice; 
+    if (400 > distanceTravelledInFeet(start, destination)) {
+        farePrice = 0
+}
+    if ((400 < distanceTravelledInFeet(start, destination) && (2000 > distanceTravelledInFeet(start, destination)))) {
+        farePrice = (.02 * (distanceTravelledInFeet(start, destination) - 400))
+}
+    if (2000 < distanceTravelledInFeet(start, destination)) { 
+        farePrice = 25
+    }
+    if (2500 < distanceTravelledInFeet(start, destination)) { 
+        farePrice = 'cannot travel that far'
+    }
+return farePrice;  
+  }
